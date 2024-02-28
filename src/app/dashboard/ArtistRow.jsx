5da5ba5abfdeff3c1bc07d3244c22d29583c2f23
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2024-02-27 19:33:30 -0700

    Loading in song and Artist data

--- src/app/dashboard/ArtistRow.jsx
+++ src/app/dashboard/ArtistRow.jsx
@@ -1,10 +1,11 @@
-export default function ArtistRow({number, song, artist, album, picture}){
+export default function ArtistRow({number, song, artist, picture}){
     return(
         <div className="flex flex-row justify-between p-2">
             <p className="w-[70px]">{number}</p>
-            <p className="w-[400px]">{artist}</p>
-            <p className="w-[400px]">{song}</p>
+            <p className="w-[300px]">{artist}</p>
+            <p className="w-[300px]">{song}</p>
             <p className="w-[200px]">{picture}</p>
+            <p className="w-[70px]"> </p>
         </div>
     )
 }
--- src/app/dashboard/Artists.jsx
+++ src/app/dashboard/Artists.jsx
@@ -1,40 +1,12 @@
 import ArtistRow from "./ArtistRow"
+import topArtists from "./topArtists.json"
+
 export default function Artists(){
     return(
         <div className="flex flex-col">
             <ArtistRow number="" artist="Artist" song="Most Played Song" picture="Artist Photo"/>
             <div class="w-full border-t border-gray-200"></div>
-            <ArtistRow number="1" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="2" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="3" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="4" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="5" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="6" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="7" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="8" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="9" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="10" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="11" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="12" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="13" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="14" song="firstSong" artist="firstartist" picture="picture"/>
-            <ArtistRow number="15" song="firstSong" artist="firstartist" picture="picture"/>
+            {topArtists.map((artist, index) => <ArtistRow number={index+1} artist={artist.artist} song={artist.mostPlayedSong} picture={artist.artistPhoto}/>)}
         </div>
     )
 }
-
-/*     
-          <div className='flex'>
-            <div className='flex flex-col'>
-                <h1>Top Artists</h1>
-                <div>
-                  <p>1. Artist 1</p>
-                  <p>2. Artist 2</p>
-                  <p>3. Artist 3</p>
-                  <p>4. Artist 4</p>
-                  <p>5. Artist 5</p>
-                </div>
-              </div>
-            <Image className='object-contain' src="/images/somethingInTheOrange.jpeg" width={100} height={100}/>
-          </div>
-        </div>*/

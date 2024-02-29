e0a667b55d8df0251524e75ab0fd1802346f0940
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2024-02-28 20:11:07 -0700

    Added Loading page for friend dashboard

--- src/app/dashboard/friends/[username]/page.jsx
+++ src/app/dashboard/friends/[username]/page.jsx
@@ -5,7 +5,9 @@ import fakeUsers from '../fakeUsers.json'
 
 export default function FriendDashboard({ params: { username }}){
     const user = fakeUsers.find(fakeUser => fakeUser.username == username)
-
+    if (!user) {
+        return <div>Loading...</div>; // Or any other placeholder you prefer
+      }
     return(
         <PageInfo title={`${user.firstName} ${user.lastName}'s Dashboard`} backButton={true}>
             <UserDashboard user={user} />

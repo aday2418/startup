91e2ce8edfe14a0fbd1540b3284c18823d6a4710
Alison Day <alisonday@Alisons-MacBook-Pro-4.local>
2024-04-03 21:42:42 -0600

    Fixed last GET error for Artists

--- src/app/api/spotify/artists/route.js
+++ src/app/api/spotify/artists/route.js
@@ -1,27 +1,27 @@
 import fetchAndUpdateArtists from "../../../../lib/fetchAndUpdateArtists"
 import getUser from "../../../../lib/getUser"
 import userIdFromToken from "../../../../lib/userIdFromToken"
-
+import { cookies } from "next/headers"
 
 
 export async function GET(request) {
-    //const url = new URL(request.url);
-    //const timeFrame = url.searchParams.get("timeframe");
     const userId = userIdFromToken()
     const user = await getUser(userId)
 
-    let artists
-    if(user?.shortArtists && user?.mediumArtists && user?.longArtists) { 
-        artists = {
-            short_term: user.shortArtists,
-            medium_term: user.mediumArtists,
-            long_term: user.longArtists
-        }
-
-        fetchAndUpdateArtists(userId)
-    } else {
-        artists = await fetchAndUpdateArtists(userId)
+    let artists = {
+        short_term: user.shortArtists,
+        medium_term: user.mediumArtists,
+        long_term: user.longArtists
     }
+
+    const cookieStore = cookies()
+    const provider_token = cookieStore.get('providerToken')
+
+    if(provider_token) { 
+        const res = await fetchAndUpdateArtists(userId, provider_token)
+        if (res)
+            artists = res
+    } 
 
     return Response.json({ data: artists})
 }
--- src/lib/fetchAndUpdateArtists.js
+++ src/lib/fetchAndUpdateArtists.js
@@ -2,22 +2,25 @@ import { cookies } from "next/headers"
 import fetchSpotify from "./fetchSpotify"
 import updateArtists from "./updateArtists"
 
-export default async function fetchAndUpdateArtists(userId, timeFrame){
-    const cookieStore = cookies()
-    const provider_token = cookieStore.get('providerToken')
-    const shortData = await fetchSpotify(provider_token.value, `/top/artists?offset=0&limit=50&time_range=short_term`)
-    const mediumData = await fetchSpotify(provider_token.value, `/top/artists?offset=0&limit=50&time_range=medium_term`)
-    const longData = await fetchSpotify(provider_token.value, `/top/artists?offset=0&limit=50&time_range=long_term`)
-    const shortArtists = shortData.items.map(item => {return {name: item.name, image: item.images[0], genres: item.genres }})
-    const mediumArtists = mediumData.items.map(item => {return {name: item.name, image: item.images[0], genres: item.genres }})
-    const longArtists = longData.items.map(item => {return {name: item.name, image: item.images[0], genres: item.genres }})
+export default async function fetchAndUpdateArtists(userId, provider_token){
+    if(provider_token){
+        const cookieStore = cookies()
+        const provider_token = cookieStore.get('providerToken')
+        const shortData = await fetchSpotify(provider_token.value, `/top/artists?offset=0&limit=50&time_range=short_term`)
+        const mediumData = await fetchSpotify(provider_token.value, `/top/artists?offset=0&limit=50&time_range=medium_term`)
+        const longData = await fetchSpotify(provider_token.value, `/top/artists?offset=0&limit=50&time_range=long_term`)
+        const shortArtists = shortData.items.map(item => {return {name: item.name, image: item.images[0], genres: item.genres }})
+        const mediumArtists = mediumData.items.map(item => {return {name: item.name, image: item.images[0], genres: item.genres }})
+        const longArtists = longData.items.map(item => {return {name: item.name, image: item.images[0], genres: item.genres }})
 
-    const artists = {
-        short_term: shortArtists,
-        medium_term: mediumArtists,
-        long_term: longArtists
+        const artists = {
+            short_term: shortArtists,
+            medium_term: mediumArtists,
+            long_term: longArtists
+        }
+        await updateArtists(userId, shortArtists, mediumArtists, longArtists)
+
+        return artists
     }
-    await updateArtists(userId, shortArtists, mediumArtists, longArtists)
-
-    return artists
+    
 }
